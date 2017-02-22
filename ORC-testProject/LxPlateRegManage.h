cd6cbf969bb8bc7f1ca00252039b4d7d59dc95d7
DavinVLee <970969044@qq.com>
2017-02-22 21:51:55 +0800

    lib add

---
+++ ORC-testProject/LxPlateRegManage.h
@@ -0,0 +1,13 @@
+//
+//  LxPlateRegManage.h
+//  ORC-testProject
+//
+//  Created by 李翔 on 17/2/22.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LxPlateRegManage : NSObject
+
+@end
---
+++ ORC-testProject/LxPlateRegManage.m
@@ -0,0 +1,13 @@
+//
+//  LxPlateRegManage.m
+//  ORC-testProject
+//
+//  Created by 李翔 on 17/2/22.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import "LxPlateRegManage.h"
+
+@implementation LxPlateRegManage
+
+@end
